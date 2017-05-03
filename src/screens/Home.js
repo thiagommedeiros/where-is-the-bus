7a67cde33deab79849d1d622a12a434f8b5d631c
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-05-03 00:17:16 -0300

    Adds a map in home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { HeaderBar } from '../components'
+import React, { Component } from 'react'
+import { HeaderBar, Map } from '../components'
+import { geolocation } from '../helpers'
 
-const Home = () => (
-  <div>
-    <HeaderBar />
-  </div>
-)
+class Home extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      lat: 0,
+      lng: 0
+    }
+  }
+
+  componentWillMount () {    
+    geolocation().then(pos => {
+      this.setState({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
+      })
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <HeaderBar />
+        <Map
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
+      </div>
+    )
+  }
+}
 
 export default Home
